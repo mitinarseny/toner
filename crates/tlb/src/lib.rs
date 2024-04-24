68bfbcd1b5cb553401e19615cfac3c32e77d4907
Arseny Mitin <mitinarseny@gmail.com>
2024-04-24 18:03:34 +0300

    fix: specify versions for dependencies

--- crates/tlb/src/lib.rs
+++ crates/tlb/src/lib.rs
@@ -6,6 +6,8 @@ mod ser;
 #[cfg(test)]
 mod tests;
 
-pub use self::{cell::*, de::*, either::*, r#as::*, ser::*};
+pub use self::{cell::*, de::*, r#as::*, ser::*};
 
 pub use tlbits::{self as bits, *};
+
+pub use ::either::Either;
