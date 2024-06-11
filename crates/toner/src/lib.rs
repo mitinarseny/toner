b1fbb188752ee79551737a726e0812404df30a19
Arseny Mitin <mitinarseny@gmail.com>
2024-06-11 21:32:16 +0400

    fix(docs): include paths

--- crates/toner/src/lib.rs
+++ crates/toner/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../../README.md"))]
+#![doc = include_str!(concat!("../", env!("CARGO_PKG_README")))]
 pub use tlb;
 pub use tlb_ton as ton;
 pub use ton_contracts as contracts;
